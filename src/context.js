ec789621b14d0d465c7ecc9a8ef6660145c332d2
Erhan <erhantezer68@gmail.com>
2023-03-23 23:13:32 +0300

    updated

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = createContext();
 
@@ -9,13 +10,34 @@ export const useGlobalContext =() => {
 export const AppProvider = ({children}) => {
     const [query, setQuery] = useState("");
     const [selectType, setSelectType] = useState("all");
-    const [data, setData] = useState([]);
+    const [myData, setMyData] = useState([]);
 
     const API_KEY = 'AIzaSyCdjHYd9EaTC8Oj4K9XZkVdODJm6-TXtos';
     const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&printType=${selectType}&key=${API_KEY}`;
-    
+
+    const getData = async () => {
+        try {
+            const {data} = await axios.get(url)
+            setMyData(data.items) //! data içerisindekii item array içinde veriler
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
+
     return (
-            <AppContext.Provider value={{}}>
+            <AppContext.Provider value={{
+            getData, 
+            query, 
+            setQuery, 
+            selectType,
+            setSelectType, 
+            myData, 
+            setMyData
+            }}>
                 {children}
             </AppContext.Provider>
         )
