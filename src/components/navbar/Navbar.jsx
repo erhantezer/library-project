c69db3aa8f4fefe97a0ded62cfe45691a2a2b324
Erhan <erhantezer68@gmail.com>
2023-03-23 21:18:20 +0300

    navbar updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,33 @@
-import { Nav } from "./Navbar.style"
+import { useState } from "react";
+import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from "./Navbar.style"
+import { menuIcon } from "../../helper/iconData";
 
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const currentUser = true;
+
     return (
         <Nav justify="space-beetwen" wrap="wrap">
+            <Logo to="/">Erhan Library</Logo>
+            <HamburgerIcon onClick={() => setShowMenu(!showMenu)}>
+                {menuIcon}
+            </HamburgerIcon>
+            <Menu showMenu={showMenu}>
+                <MenuLink to="/">Home</MenuLink>
+                <MenuLink to="/about">About</MenuLink>
 
+                {currentUser ? (
+                    <MenuLink to="/login">
+                        Logout
+                    </MenuLink>
+                ) : (
+                    <>
+                        <MenuLink to="/register">Register</MenuLink>
+                        <MenuLink to="login">Login</MenuLink>
+                    </>
+                )}
+            </Menu>
         </Nav>
     )
 }
