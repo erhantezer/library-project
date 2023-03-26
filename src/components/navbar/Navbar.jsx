9e1e84250654b1531689b6b0de7b1fb68e4af222
Erhan <erhantezer68@gmail.com>
2023-03-26 14:06:51 +0300

    updated project

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from "./Navbar.style";
 import { menuIcon } from "../../helper/iconData";
 
-
-const Navbar = () => {
+const Navbar = ({ currentUser, setCurrentUser }) => {
     const [showMenu, setShowMenu] = useState(false);
-    const currentUser = true;
+
+    const logout = () => {
+        setCurrentUser(false);
+        sessionStorage.clear();
+    };
 
     return (
-        <Nav justify="space-beetwen" wrap="wrap">
-            <Logo to="/">Erhan Library</Logo>
+        <Nav justify="space-between" wrap="wrap">
+            <Logo to="/">Clarus Library</Logo>
             <HamburgerIcon onClick={() => setShowMenu(!showMenu)}>
                 {menuIcon}
             </HamburgerIcon>
             <Menu showMenu={showMenu}>
                 <MenuLink to="/">Home</MenuLink>
                 <MenuLink to="/about">About</MenuLink>
+                {/* <MenuLink to="/register">Register</MenuLink>
+        <MenuLink to="/login">Login</MenuLink>
+        <MenuLink to="/login">Logout</MenuLink> */}
+
                 {currentUser ? (
-                    <MenuLink to="/login">
+                    <MenuLink to="/login" onClick={logout}>
                         Logout
                     </MenuLink>
                 ) : (
                     <>
                         <MenuLink to="/register">Register</MenuLink>
-                        <MenuLink to="login">Login</MenuLink>
+                        <MenuLink to="/login">Login</MenuLink>
                     </>
                 )}
             </Menu>
         </Nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
