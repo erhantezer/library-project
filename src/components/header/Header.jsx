12d2f3b9e689332cfa644df7526ed0200795b5bd
Erhan <erhantezer68@gmail.com>
2023-03-26 13:34:04 +0300

    header.jsx updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { HeaderButton, HeaderContainer, HeaderForm, HeaderTitle, SearchInput, SelectBox } from './Header.style'
+import { useGlobalContext } from '../../context';
 
 const Header = () => {
+    const { setQuery, setSelectType, getData } = useGlobalContext()
+    const printType = ["all", "books", "magazines"];
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getData();
+    }
+
     return (
-        <div>Header</div>
+        <HeaderContainer>
+            <HeaderTitle>Books or Magazines</HeaderTitle>
+            <HeaderForm onSubmit={handleSubmit}>
+                <SearchInput type="text" placeholder='name' onChange={(e) => setQuery(e.target.value)} required />
+                <SelectBox onChange={(e) => setSelectType(e.target.value)}>
+                    {
+                        printType.map((type) => {
+                            return <option key={type} value={type}>{type}</option>;
+                        })
+                    }
+                </SelectBox>
+                <HeaderButton type='submit'>
+                    Search
+                </HeaderButton>
+            </HeaderForm>
+        </HeaderContainer>
     )
 }
 
